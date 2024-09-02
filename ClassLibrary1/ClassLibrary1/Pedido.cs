8a41fa16f3c691635b85911c71e5f42db1a7a4f3
SantiResFonn <159478006+SantiResFonn@users.noreply.github.com>
2024-09-02 17:20:01 -0500

    Clases bases
    Creación de clases bases del problema

---
+++ ClassLibrary1/ClassLibrary1/Pedido.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Pedido
+    {
+        public int Id { get; set; } = 1;
+
+        public float Propina { get; set; }
+
+        public List<string> Productos_pedido { get; set; }
+
+        public int Numero_mesa { get; set; }
+
+   
+        public Pedido(List<string> productos, int numero_mesa) {
+            Id++;
+            Productos_pedido = productos;
+            Numero_mesa = numero_mesa;
+            Propina = 0;
+        }
+    }
+}
